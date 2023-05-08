b16bcece790683dbd42d34b2f25314f7a3f35870
Baiba Rozenštoka <brozenstoka@edu.riga.lv>
2023-05-08 21:52:23 +0300

    new app test

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello():
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run()
