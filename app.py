41aa772c7d9417ebe8ed000bcbff3abc849118d9
Baiba Rozenštoka <brozenstoka@edu.riga.lv>
2023-05-09 00:58:02 +0300

    please for the love of god STRAADAA

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
