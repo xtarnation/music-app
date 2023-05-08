62cd350dc3c334c6dca932ec705980e52ce99396
Baiba Rozenštoka <brozenstoka@edu.riga.lv>
2023-05-08 20:33:07 +0300

    basic structure

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+if __name__ == "__main__":
+    app.run(debug=True)
