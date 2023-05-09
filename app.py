b4654faa9f3bfc11a942f05b368a8589f1e2106a
Baiba Rozenštoka <brozenstoka@edu.riga.lv>
2023-05-09 08:58:11 +0300

    NAVIGATION BAR

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/music')
+def music():
+    return render_template('music.html')
+
+@app.route('/artists')
+def artists():
+    return render_template('artists.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
