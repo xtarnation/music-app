5b94872aa08cd940fff91ea64be1b4fd0826d193
Baiba Rozenštoka <brozenstoka@edu.riga.lv>
2023-05-09 21:01:01 +0300

    profile cards

--- app.py
+++ app.py
@@ -3,35 +3,46 @@ import sqlite3
 
 import dbconnect
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/music')
 def music():
     conn = dbconnect.connect()
     c = conn.cursor()
-
     songs_sqliterow = c.execute('SELECT * FROM songs').fetchall()
+    dbconnect.close(conn)
+
     songs_dict = []
     for one in songs_sqliterow:
         songs_dict.append(dict(one))
 
-    length = len(songs_dict)
-    return render_template('music.html', songs=songs_dict, length=length)
+    return render_template('music.html', songs=songs_dict)
 
-@app.route('/artists', methods=['GET', 'POST'])
+
+@app.route('/artists')
 def artists():
-    if request.method == 'POST':
-        artist = request.form['artist']
-        return redirect(url_for('artist', artist=artist))
-    return render_template('artists.html')
+    conn = dbconnect.connect()
+    c = conn.cursor()
+    artists_sqliterow = c.execute('SELECT * FROM artists').fetchall()
+    dbconnect.close(conn)
+
+    artists_dict = []
+    for one in artists_sqliterow:
+        artists_dict.append(dict(one))
+
+    return render_template('artists.html', artists=artists_dict)
+
 
 @app.route('/artists/<artist>')
 def artist(artist):
     return render_template('artistprofile.html', artist=artist)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
